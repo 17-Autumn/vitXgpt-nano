2061d0f6e8cbde2e0a0fd4f38942c3886e9965c3
17-Autumn <codehochai@gmail.com>
2025-10-30 00:41:50 +0700

    Update retrain_v1.py

--- retrain_v1.py
+++ retrain_v1.py
@@ -812,7 +812,7 @@ def main():
         'num_registers': 4,
         
         # Training hyperparameters (giữ nguyên như code 1)
-        'batch_size': 38,  # Per GPU
+        'batch_size': 32,  # Per GPU
         'epochs': 12,
         'lr': 1e-4,
         'weight_decay': 0.02,
